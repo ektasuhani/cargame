5578cd283a20d792c38b1bb8f672f6385e90f1e1
ektasuhani <suhanijain29aug@gmail.com>
2020-12-21 13:59:59 +0530

    playing game

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,7 @@ class Game {
 
   getState(){
     var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
+    gameStateRef.on("value",(data)=>{
        gameState = data.val();
     })
 
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("suhani",car1img)
     car2 = createSprite(300,200);
+    car2.addImage("ayush",car2img);
     car3 = createSprite(500,200);
+    car3.addImage("vriddhi",car3img);
     car4 = createSprite(700,200);
+    car4.addImage("govind",car4img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background ("black");
+      image (trackimg,0,-displayHeight*4,displayWidth,displayHeight*5)
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -77,8 +82,14 @@ class Game {
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
       player.update();
+      console.log(player.distance)
     }
-
+if(player.distance>4130){
+  gameState=2
+}
     drawSprites();
+  }
+  end(){
+    console.log("game ended");
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var canvas, backgroundImage;
 
 var gameState = 0;
-var playerCount;
+var playerCount=0;
 var allPlayers;
 var distance = 0;
 var database;
@@ -9,7 +9,13 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+function preload(){
+car1img=loadImage("images/car1.png");
+car2img=loadImage("images/car2.png");
+car3img=loadImage("images/car3.png");
+car4img=loadImage("images/car4.png");
+trackimg=loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +33,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end()
   }
 }
